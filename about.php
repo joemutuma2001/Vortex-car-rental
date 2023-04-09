8c143a11a592b35897ff8a49a048cd0382b0def4
joemutuma2001 <109172044+joemutuma2001@users.noreply.github.com>
2023-04-09 17:01:29 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Vortex Rental | About Us</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <meta content="Author" name="WebThemez">
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet"> 
+</head>
+
+<body id="body"> 
+ <?php include('includes/header.php');?>
+ <section id="innerBanner"> 
+  <div class="inner-content">
+    <h2><span>About Us</span><br>We provide high quality cars</h2>
+    <div> 
+    </div>
+  </div> 
+</section><!-- #Page Banner -->
+
+<main id="main">
+      <section id="about" class="wow fadeInUp">
+        <div class="container"> 
+          <div class="row">
+            <div class="col-lg-6 about-img">
+              <img src="img/nissan.jpg" alt="">
+            </div>
+
+            <div class="col-lg-6 content">
+              <h2>Vortex has a new face. We decided to give a fresher look to our brand and our services. With our fully renewed fleet of vehicles, we are ready to meet all expectations and requirements.</h2>
+              <h3>Why choose us?</h3>
+              <ul>
+                <li><i class="icon ion-ios-checkmark-outline"></i>  If you want to book directly through a supplier, and not through a broker – choose Vortex Rental</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>   this will give you better flexibility in terms of vehicle choices;</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>  vehicle make and model will be confirmed, and not “similar” to those you selected;</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>  you can directly negotiate some of the terms and conditions, payment options, especially if you require unique or long term rental service;</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>  you can book “commission free”;</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>   you can reach us 24/7 on our mobile numbers;</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>   you can call us free from the “Free call” service on our website;</li>
+
+
+              </ul> 
+            </div>
+          </div>
+
+        </div>
+      </section><!-- #about -->
+    </main>
+    <?php include('includes/footer.php');?>
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript  -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script> 
+    <script src="contact/jqBootstrapValidation.js"></script>
+    <script src="contact/contact_me.js"></script>
+    <script src="js/main.js"></script>
+
+  </body>
+  </html>
