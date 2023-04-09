d491e95f8a95812d39cc9c76939edbf8a1eb816f
joemutuma2001 <109172044+joemutuma2001@users.noreply.github.com>
2023-04-09 17:04:40 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong>Vortex Rental</strong>. All Rights Reserved
+    </div>
+  </div>
+</footer>
